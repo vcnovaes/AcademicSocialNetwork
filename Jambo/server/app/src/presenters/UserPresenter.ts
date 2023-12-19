7d2b500379219e36e1926ae4b996e979c2d1e731
vcnovaes <vcn2@cin.ufpe.br>
2023-12-19 17:11:51 -0300

    encapsulando auth

--- Jambo/server/app/src/presenters/UserPresenter.ts
+++ Jambo/server/app/src/presenters/UserPresenter.ts
@@ -4,6 +4,7 @@ import { HttpStatusCode } from "../communication/http/HTTPStatusCode";
 import { Jambo } from "..";
 import cookie from "@elysiajs/cookie";
 import { Jwt } from "../communication/http/Jwt";
+import { Auth } from "../security/Auth";
 
 export const UserPresenter = new Elysia().group("user", (app) =>
   app
@@ -44,19 +45,16 @@ export const UserPresenter = new Elysia().group("user", (app) =>
 
     .use(Jwt.jwt())
     .use(cookie())
+
     .post("/login", async ({ jwt, cookie, setCookie, body, set }) => {
-      const { email, password } = body as any;
-      const user = await Jambo.verifyUserIdentity(email, password);
-      const x = JSON.stringify({ email: user?.email }) as any;
-      if (user) {
-        setCookie("auth", await jwt.sign(body as any), {
-          maxAge: 7200,
-        });
+      const auth = await Auth.startSession(body, setCookie, jwt, cookie);
+      if (auth) {
         set.status = 200;
         return cookie.auth;
       }
       set.status = HttpStatusCode.BAD_REQUEST;
     })
+
     .get("/test", async ({ jwt, set, cookie: { auth } }) => {
       const profile = await jwt.verify(auth);
 
@@ -88,22 +86,22 @@ export const UserPresenter = new Elysia().group("user", (app) =>
     })
 
     .delete("/", async ({ jwt, set, cookie: { auth } }) => {
-      const { email } = (await jwt.verify(auth)) as any;
+      const email = await Auth.verifySession(auth, jwt);
       if (!email) {
-        set.status = HttpStatusCode.UNAUTHORIZED;
-        return "Unauthorized";
+        set.status = HttpStatusCode.FORBIDDEN;
+        return;
       }
       try {
-        await Jambo.deleteUser(email);
+        await Jambo.deleteUser(email as string);
       } catch (err) {
         console.error("Error updating user: ", email);
         console.error(err);
         throw new InternalServerError();
       }
     })
+
     .get("/", async ({ jwt, cookie: { auth } }) => {
-      const { email } = (await jwt.verify(auth)) as any;
-      console.info(email);
+      const email = await Auth.verifySession(auth, jwt);
       const userData = await Jambo.getUser(email);
       if (!userData) {
         throw new NotFoundError();
