36d114f202e69c7c1e1782667efb00eecdea67ad
vcnovaes <vcn2@cin.ufpe.br>
2023-12-19 18:33:53 -0300

    redirect working

--- Jambo/client/jambo-client/src/App.tsx
+++ Jambo/client/jambo-client/src/App.tsx
@@ -1,31 +1,61 @@
 // src/App.tsx
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignUpForm from './components/SignUpForm';
-import ConfirmationForm from './components/EmailConfirmationForm'
-import LoginForm from './components/LoginForm'
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  NavigateFunction,
+} from "react-router-dom";
+import SignUpForm from "./components/SignUpForm";
+import ConfirmationForm from "./components/EmailConfirmationForm";
+import LoginForm from "./components/LoginForm";
+import Feed from "./components/Feed";
+import { Navigate } from "react-router-dom";
+import UserProfileForm from "./components/UserDataForm";
 
 function App() {
   const [userEmail, setUserEmail] = useState<string | null>(null);
-
+  let isAuthenticated = document.cookie.includes("auth");
+  console.log(isAuthenticated);
   const handleUserSignUp = (email: string) => {
     // Set the user's email in the state when signing up
     setUserEmail(email);
   };
-  const handleUserLogin = (email: string, password: string) => {
+  const handleUserLogin = (logged: boolean, navigate: NavigateFunction) => {
     // Handle login logic here
-    console.log(`User logged in with email: ${email}, password: ${password}`);
+    isAuthenticated = true;
+    navigate("/feed");
+    console.log("user logged");
   };
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <SignUpForm onSignUp={handleUserSignUp} />
-            <LoginForm onLogin={handleUserLogin} />
-          </div>} />
-          {/* Pass handleUserSignUp as a callback to SignUpForm */}
-        <Route path="/confirmation" element= {userEmail && <ConfirmationForm userEmail={userEmail}/>}/> 
+        <Route
+          path="/"
+          element={
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <SignUpForm onSignUp={handleUserSignUp} />
+              <LoginForm onLogin={handleUserLogin} />
+            </div>
+          }
+        />
+        {/* Pass handleUserSignUp as a callback to SignUpForm */}
+        <Route
+          path="/confirmation"
+          element={userEmail && <ConfirmationForm userEmail={userEmail} />}
+        />
+        <Route
+          path="/feed"
+          Component={() => (isAuthenticated ? <Feed /> : <Navigate to="/" />)}
+        />
+        <Route
+          path="/user"
+          Component={() =>
+            isAuthenticated ? <UserProfileForm /> : <Navigate to="/" />
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ Jambo/client/jambo-client/src/components/Feed.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Feed: React.FC = () => {
+  return (
+    <div>
+      <h2>Private Page</h2>
+      <p>
+        Welcome to the private page! You can only access this page when logged
+        in.
+      </p>
+      {/* Add your private page content here */}
+    </div>
+  );
+};
+
+export default Feed;
