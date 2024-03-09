079e325e2593d919aa1fbd195469a9c9bf761bbb
becca-ls <rls7@cin.ufpe.br>
2024-03-09 20:38:13 -0300

    feature de deleção do post funcionando

--- backend/services/posts/data/posts.py
+++ backend/services/posts/data/posts.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from typing import Optional
 from uuid import uuid4
 from configuration.db import Base
+from fastapi import FastAPI, HTTPException
 
 class PostsORM(Base):
     __tablename__ = 'posts'
@@ -27,9 +28,23 @@ async def create_posts(db_session: Session, posts: PostsModel) -> PostsORM:
     db_session.add(posts.orm())
     db_session.commit()
 
+
+async def delete_post_by_id(db_session: Session, post_id: int) -> None:
+    post = db_session.query(PostsORM).filter(PostsORM.id == post_id).first()
+    if not post:
+       raise HTTPException(status_code=404, detail="Post not found")
+    db_session.delete(post)
+    db_session.commit()
+
+
 async def get_posts(db_session: Session, id: str) -> PostsORM:
     query = db_session.query(PostsORM).filter(PostsORM.id == id)
     return db_session.execute(query).scalars().first()
+
+def get_all_posts_by_owner(db_session: Session, owner: str) -> list[PostsORM]:
+    query = db_session.query(PostsORM).filter(PostsORM.owner == owner)
+    posts = db_session.execute(query).all()
+    return posts
 
 async def update_posts(db_session: Session, posts: PostsModel):
     query = db_session.query(PostsORM).filter(
